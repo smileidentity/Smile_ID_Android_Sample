df0a3933057b6224b8f92d0c448b45c2d082a1cd
arlonmukeba <arlon.mukeba@medable.com>
2021-02-13 14:31:12 +0200

    Rebase with develop

--- app/src/main/java/com/demo/smileid/sid_sdk/SIDSplashActivity.java
+++ app/src/main/java/com/demo/smileid/sid_sdk/SIDSplashActivity.java
@@ -3,15 +3,11 @@ package com.demo.smileid.sid_sdk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.smileidentity.libsmileid.utils.Version;
-
 
 public class SIDSplashActivity extends AppCompatActivity {
 
